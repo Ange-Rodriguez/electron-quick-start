678c6d4b668991ec6b07abac4cfacf7c3bd2a755
Ange Rodriguez <ange.rodriguez@thunderwave.fr>
2021-01-03 22:40:57 +0100

    build : loadURL

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('http://trello.com')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
